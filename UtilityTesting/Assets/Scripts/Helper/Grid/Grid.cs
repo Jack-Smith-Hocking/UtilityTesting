d882bb9a1f909156b9ff7633602e10b54f13611f
Jack Smith-Hocking <s191037@students.aie.edu.au>
2021-05-18 00:30:06 +0930

    Implemented a grid system
      - Supports a templated grid of any size, and any type
      - GridVisualiser for easier debugging

---
+++ UtilityTesting/Assets/Scripts/Helper/Grid/Grid.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Helper.Utility;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+namespace Helper.Grid
+{
+    public class Grid<TGridObject>
+    {
+        public event System.Action<int, int> OnGridObjectChanged;
+
+        public int Height { get; private set; } = 0;
+        public int Width { get; private set; } = 0;
+        public float CellSize { get; private set; } = 0;
+
+        public Vector3 Origin { get; private set; } = Vector3.zero;
+
+        private TGridObject[,] m_gridArr;
+
+        // Public //
+
+        public Grid(int width, int height, float cellSize, Vector3 origin, TGridObject defaultVal = default) => InitGrid(width, height, cellSize, origin, (gridObject, tempWidth, tempHeight) => defaultVal);
+        public Grid(int width, int height, float cellSize, Vector3 origin, System.Func<Grid<TGridObject>, int, int, TGridObject> defaultVal) => InitGrid(width, height, cellSize, origin, defaultVal);
+
+        public bool GetGridObject(int x, int y, out TGridObject returnObject)
+        {
+            returnObject = default;
+            if (IsValidInput(x, y) == false) return false;
+
+            returnObject = m_gridArr[x, y];
+            return true;
+        }
+        public bool GetGridObject(Vector2Int gridPosition, out TGridObject returnObject) => GetGridObject(gridPosition.x, gridPosition.y, out returnObject);
+        public bool GetGridObject(Vector3 worldPosition, out TGridObject returnObject) => GetGridObject(ToGridPosition(worldPosition), out returnObject);
+
+        public void SetGridObject(int x, int y, TGridObject value)
+        {
+            if (IsValidInput(x, y) == false) return;
+
+            m_gridArr[x, y] = value;
+
+            OnGridObjectChanged?.Invoke(x, y);
+        }
+        public void SetGridObject(Vector2Int gridPosition, TGridObject value) => SetGridObject(gridPosition.x, gridPosition.y, value);
+        public void SetGridObject(Vector3 worldPosition, TGridObject value) => SetGridObject(ToGridPosition(worldPosition), value);
+
+        public void TriggerGridObjectChanged(int x, int y)
+        {
+            if (IsValidInput(x, y) == false) return;
+
+            OnGridObjectChanged?.Invoke(x, y);
+        }
+
+        public Vector3 ToCentredWorldPosition(int x, int y) => ToWorldPosition(x, y) + (new Vector3(CellSize, CellSize) * 0.5f);
+        public Vector3 ToCentredWorldPosition(Vector2Int gridPosition) => ToCentredWorldPosition(gridPosition.x, gridPosition.y);
+        public Vector3 ToCentredWorldPosition(Vector3 worldPosition) => ToCentredWorldPosition(ToGridPosition(worldPosition));
+
+        public Vector3 ToWorldPosition(int x, int y) => (new Vector3(x, y) * CellSize) + Origin;
+        public Vector3 ToWorldPosition(Vector2Int gridPosition) => ToWorldPosition(gridPosition.x, gridPosition.y);
+
+        public Vector2Int ToGridPosition(Vector3 worldPosition)
+        {
+            Vector3 _pos = worldPosition - Origin;
+            int _x = Mathf.FloorToInt(_pos.x / CellSize);
+            int _y = Mathf.FloorToInt(_pos.y / CellSize);
+
+            return new Vector2Int(_x, _y);
+        }
+
+        public void LoopThroughGrid(System.Action<int, int> loopAction)
+        {
+            if (loopAction.IsNull()) return;
+
+            for (int _x = 0; _x < Width; _x++)
+            {
+                for (int _y = 0; _y < Height; _y++)
+                {
+                    loopAction?.Invoke(_x, _y);
+                }
+            }
+        }
+        
+        // Private //
+
+        private void InitGrid(int width, int height, float cellSize, Vector3 origin, System.Func<Grid<TGridObject>, int, int, TGridObject> defaultVal)
+        {
+            Width = Mathf.Clamp(width, 0, int.MaxValue);
+            Height = Mathf.Clamp(height, 0, int.MaxValue);
+            CellSize = Mathf.Clamp(cellSize, 0, float.MaxValue);
+
+            Origin = origin;
+
+            m_gridArr = new TGridObject[Width, Height];
+
+            LoopThroughGrid((x, y) =>
+            {
+                m_gridArr[x, y] = defaultVal.Invoke(this, Width, Height);
+            });
+        }
+
+        private bool IsValidInput(int x, int y) => x.In(0, Width - 1) && y.In(0, Height - 1);
+    }
+}
---
+++ UtilityTesting/Assets/Scripts/Helper/Grid/GridVisualiser.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Helper.Utility;
+using Helper.Updater;
+using Sirenix.OdinInspector;
+
+namespace Helper.Grid
+{
+    public class GridVisualiser : MonoBehaviour
+    {
+        [TabGroup("Grid")]
+        public Transform m_gridBackPlane;
+
+        [Space]
+
+        [TabGroup("Grid")]
+        public int m_width;
+
+        [TabGroup("Grid")]
+        public int m_height;
+
+        [TabGroup("Grid")]
+        public float m_cellSize;
+
+        // //
+
+        [TabGroup("Debug")]
+        public bool m_showGridDebug = true;
+
+        [Space]
+
+        [TabGroup("Debug")]
+        [ShowIf(nameof(m_showGridDebug))]
+        public Transform m_debugParent;
+
+        [TabGroup("Debug")]
+        [ShowIf(nameof(m_showGridDebug))]
+        public GameObject m_textMeshPrefab;
+
+        private Grid<int> m_testIntGrid;
+
+        private void Start()
+        {
+            m_testIntGrid = new Grid<int>(m_width, m_height, m_cellSize, transform.position, 0);
+            m_testIntGrid.SetGridObject(0, 0, 56);
+
+            if (m_showGridDebug == false) return;
+
+            GridDebugInit(m_testIntGrid, m_textMeshPrefab, m_debugParent);
+        }
+
+        private void Update()
+        {
+            MouseHover();
+            MouseClick();
+        }
+
+        private void MouseClick()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit _rayHit)) return;
+
+                Vector3 _gridPos = _rayHit.point;
+
+                m_testIntGrid.GetGridObject(_gridPos, out int _obj);
+                m_testIntGrid.SetGridObject(_gridPos, _obj += 5);
+            }
+        }
+        private void MouseHover()
+        {
+            if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit _rayHit)) return;
+
+            Vector3 _gridPos = _rayHit.point;
+
+            m_testIntGrid.GetGridObject(_gridPos, out int _obj);
+            m_testIntGrid.SetGridObject(_gridPos, _obj += 1);
+        }
+
+        private static void GridDebugInit<T>(Grid<T> grid, GameObject prefab, Transform parent = null)
+        {
+            if (grid.IsNull()) return;
+            if (prefab.IsNull()) return;
+
+            TextMesh[,] m_textGrid = new TextMesh[grid.Width, grid.Height];
+
+            grid.LoopThroughGrid((x, y) =>
+            {
+                grid.GetGridObject(x, y, out T _obj);
+
+                GameObject _prefabInstance = Instantiate(prefab, grid.ToCentredWorldPosition(x, y), Quaternion.identity, parent);
+                m_textGrid[x, y] = _prefabInstance.ExtractComponent<TextMesh>();
+                m_textGrid[x, y].text = _obj?.ToString();
+
+                Debug.DrawLine(grid.ToWorldPosition(x, y), grid.ToWorldPosition(x, y + 1), Color.white, float.MaxValue);
+                Debug.DrawLine(grid.ToWorldPosition(x, y), grid.ToWorldPosition(x + 1, y), Color.white, float.MaxValue);
+            });
+
+            Debug.DrawLine(grid.ToWorldPosition(0, grid.Height), grid.ToWorldPosition(grid.Width, grid.Height), Color.white, float.MaxValue);
+            Debug.DrawLine(grid.ToWorldPosition(grid.Width, 0), grid.ToWorldPosition(grid.Width, grid.Height), Color.white, float.MaxValue);
+
+            grid.OnGridObjectChanged += (x, y) =>
+            {
+                grid.GetGridObject(x, y, out T _obj);
+                m_textGrid[x, y].text = _obj?.ToString();
+            };
+        }
+
+    }
+}
