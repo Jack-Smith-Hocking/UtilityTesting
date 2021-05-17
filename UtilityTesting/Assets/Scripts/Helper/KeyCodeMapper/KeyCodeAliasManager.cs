c9033ce4e90fc665ac71fbc6dc3decc055590114
Jack Smith-Hocking <s191037@students.aie.edu.au>
2021-05-18 00:28:56 +0930

    Extended the KeyCodeMapper system
      - Now supports KeyCode aliases
      - Now supports IsHeld and IsDown for primary KeyCodes

---
+++ UtilityTesting/Assets/Scripts/Helper/KeyCodeMapper/KeyCodeAliasManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+namespace Helper.Utility
+{
+    [CreateAssetMenu(menuName = "ScriptableObject/KeyCodeAliasSettings", fileName = "New KeyCodeAliasSettings")]
+    public class KeyCodeAliasManager : SerializedScriptableObject
+    {
+        public static KeyCodeAliasManager Instance => s_instance;
+        private static KeyCodeAliasManager s_instance = null;
+
+        [SerializeField] private Dictionary<KeyCode, List<KeyCode>> m_aliases = new Dictionary<KeyCode, List<KeyCode>>();
+
+        private static bool s_warningMessageSent = false;
+
+        /// <summary>
+        /// Perform an action on all the aliases that were found for a keycode
+        /// </summary>
+        /// <param name="keyCode">The KeyCode to look for aliases of</param>
+        /// <param name="loopFunction">The Func that will be run on all aliases</param>
+        /// <param name="performOnOriginal">Whether the loopFunction will be invoked on the original KeyCode</param>
+        public static void PerformOnAliases(KeyCode keyCode, System.Func<KeyCode, bool> loopFunction, bool performOnOriginal = true)
+        {
+            bool _validAliasFound = false;
+
+            if (loopFunction.IsNull()) return;
+            if (performOnOriginal) _validAliasFound = loopFunction.Invoke(keyCode);
+
+            if (s_instance.IsNull())
+            {
+                if (s_warningMessageSent == false)
+                {
+                    Debug.LogWarning("No KeyCodeAliasManager is currently set up!");
+                    s_warningMessageSent = true;
+                }
+                return;
+            }
+
+            if (s_instance.m_aliases.DoesNotContainKey(keyCode)) return;
+
+            foreach (KeyCode _alias in s_instance.m_aliases[keyCode])
+            {
+                if (_validAliasFound) return;
+
+                _validAliasFound = loopFunction.Invoke(_alias);
+            }
+        }
+
+        private void OnEnable()
+        {
+            if (s_instance.IsNotNull() && this.Equals(s_instance) == false)
+            {
+                Debug.LogWarning("There can only be one KeyCodeAliasManager asset in the project at a time");
+
+                DestroyImmediate(this, true);
+                return;
+            }
+
+            s_instance = this;
+        }
+    }
+}
--- UtilityTesting/Assets/Scripts/Helper/KeyCodeMapper/KeyCodeEventMapper.cs
+++ UtilityTesting/Assets/Scripts/Helper/KeyCodeMapper/KeyCodeEventMapper.cs
@@ -3,9 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using Sirenix.OdinInspector;
+using Helper.Updater;
 
 namespace Helper.Utility
 {
+    public enum KeyState
+    { 
+        DOWN,
+        HELD
+    }
+
     [System.Serializable]
     public struct KeyCodeEventData
     {
@@ -14,29 +21,54 @@ namespace Helper.Utility
         [TabGroup("Key")]
         public KeyCode m_primaryKeyCode;
 
+        [TabGroup("Key")]
+        public KeyState m_keyState;
+
         [TabGroup("Event")]
-        public UnityEvent m_event;
+        public UnityEvent OnActiveEvent;
+        public System.Action OnActive;
 
         [TabGroup("Description"), TextArea()]
         public string m_description;
 
-        public KeyCodeEventData(KeyCode mod, KeyCode primary, System.Action mapAction, string description = "")
+        public KeyCodeEventData(KeyCode mod, KeyCode primary, KeyState keyState, System.Action mapAction, string description = "")
         {
             m_modifierKeyCode = mod;
             m_primaryKeyCode = primary;
 
-            m_event = new UnityEvent();
-            m_event.AddListener(() => { mapAction?.Invoke(); });
+            m_keyState = keyState;
+
+            OnActiveEvent = new UnityEvent();
+            OnActive = mapAction;
 
             m_description = description;
         }
 
-        public bool IsDown()
+        public void ActivateEvents()
         {
-            bool _mod = m_modifierKeyCode == KeyCode.None || Input.GetKey(m_modifierKeyCode);
-
-            return _mod && Input.GetKeyDown(m_primaryKeyCode);
+            OnActiveEvent?.Invoke();
+            OnActive?.Invoke();
         }
+
+        public bool IsStateActive() => IsStateActive(m_modifierKeyCode, m_primaryKeyCode, m_keyState);
+        public static bool IsStateActive(KeyCode mod, KeyCode primary, KeyState keyState)
+        {
+            switch (keyState)
+            {
+                case KeyState.DOWN: return IsDown(mod, primary);
+                case KeyState.HELD: return IsHeld(mod, primary);
+
+                default: return false;
+            }
+        }
+
+        public static bool IsModDown(KeyCode mod) => mod == KeyCode.None || Input.GetKey(mod);
+
+        public bool IsDown() => IsDown(m_modifierKeyCode, m_primaryKeyCode);
+        public static bool IsDown(KeyCode mod, KeyCode primary) => IsModDown(mod) && Input.GetKeyDown(primary);
+
+        public bool IsHeld() => IsHeld(m_modifierKeyCode, m_primaryKeyCode);
+        public static bool IsHeld(KeyCode mod, KeyCode primary) => IsModDown(mod) && Input.GetKey(primary);
     }
 
     public class KeyCodeEventMapper : SerializedMonoBehaviour
@@ -61,18 +93,48 @@ namespace Helper.Utility
             {
                 foreach (KeyCodeEventData _mappedKey in m_mappedKeys)
                 {
-                    if (_mappedKey.IsDown()) _mappedKey.m_event?.Invoke();
+                    CheckKeyCodeEvent(_mappedKey);
                 }
-            }, "MappedKeyCodes", true, UpdateType.NORMAL);
+            }, "MappedKeyCodes", true, UpdateCycle.NORMAL);
         }
 
         public void AddMappedKey(KeyCodeEventData mappedKey)
         {
             m_mappedKeys.Add(mappedKey);
         }
-        public void AddMappedKey(KeyCode mod, KeyCode primary, System.Action mapAction, string description = "")
+        public void AddMappedKey(KeyCode mod, KeyCode primary, KeyState keyState, System.Action mapAction, string description = "")
         {
-            m_mappedKeys.Add(new KeyCodeEventData(mod, primary, mapAction, description));
+            m_mappedKeys.Add(new KeyCodeEventData(mod, primary, keyState, mapAction, description));
+        }
+
+        private void CheckKeyCodeEvent(KeyCodeEventData eventData)
+        {
+            bool _modIsDown = false;
+            KeyCode _validMod = KeyCode.None;
+
+            // Loop through all the aliases for 
+            KeyCodeAliasManager.PerformOnAliases(eventData.m_modifierKeyCode, (KeyCode alias) =>
+            {
+                _modIsDown = KeyCodeEventData.IsModDown(alias);
+                _validMod = alias;
+
+                return _modIsDown;
+            }, true);
+
+            if (_modIsDown == false) return;
+
+            bool _primIsDown = false;
+
+            KeyCodeAliasManager.PerformOnAliases(eventData.m_primaryKeyCode, (KeyCode alias) =>
+            {
+                _primIsDown = KeyCodeEventData.IsStateActive(_validMod, alias, eventData.m_keyState);
+
+                return _primIsDown;
+            }, true);
+
+            if (_primIsDown == false) return;
+
+            eventData.ActivateEvents();
         }
     }
 }
