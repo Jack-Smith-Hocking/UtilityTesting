616b399b314d1e8d47ecf50c44d57b9dced8864b
Jack Smith-Hocking <s191037@students.aie.edu.au>
2021-05-18 00:31:19 +0930

    Tested the stability of the improved KeyCodeMapper system
      - Created a simple camera controller using the KeyCodeMapper system

---
+++ UtilityTesting/Assets/Scripts/General/SimpleCamera2DMover.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Helper.Utility;
+using Helper.Updater;
+
+public class SimpleCamera2DMover : MonoBehaviour
+{
+    public float m_moveSpeed;
+
+    private Vector3 m_moveDir;
+
+    private Camera m_mainCam;
+
+    private void Start()
+    {
+        m_mainCam = Camera.main;
+
+        KeyCodeEventMapper.Instance.AddMappedKey(KeyCode.None, KeyCode.UpArrow, KeyState.HELD, () => { m_moveDir += Vector3.up; }, "SimpleCamera2DMover -> Update the camera");
+        KeyCodeEventMapper.Instance.AddMappedKey(KeyCode.None, KeyCode.DownArrow, KeyState.HELD, () => { m_moveDir += Vector3.down; }, "SimpleCamera2DMover -> Update the camera");
+
+        KeyCodeEventMapper.Instance.AddMappedKey(KeyCode.None, KeyCode.LeftArrow, KeyState.HELD, () => { m_moveDir += Vector3.left; }, "SimpleCamera2DMover -> Update the camera");
+        KeyCodeEventMapper.Instance.AddMappedKey(KeyCode.None, KeyCode.RightArrow, KeyState.HELD, () => { m_moveDir += Vector3.right; }, "SimpleCamera2DMover -> Update the camera");
+
+        FunctionUpdater.CreateUpdater(() => { m_moveDir.z += Input.mouseScrollDelta.y; }, "SimpleCamera2dMover - Zoom", true, UpdateCycle.NORMAL);
+    }
+
+    private void LateUpdate()
+    {
+        Move(m_moveDir);     
+    }
+
+    private void Move(Vector3 dir)
+    {
+        m_mainCam.transform.position += m_moveDir.normalized * m_moveSpeed * Time.deltaTime;
+
+        m_moveDir = Vector3.zero;
+    }
+}
--- UtilityTesting/Assets/Scripts/General/SimpleController.cs
+++ UtilityTesting/Assets/Scripts/General/SimpleController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Helper.Updater;
+using Helper.Utility;
 
 public class SimpleController : MonoBehaviour
 {
@@ -10,13 +12,28 @@ public class SimpleController : MonoBehaviour
     private Rigidbody m_rigidBody;
     private Camera m_viewCam;
 
-    private Vector3 m_velocity;
+    private Vector3 m_moveDir;
 
     // Start is called before the first frame update
     void Start()
     {
         m_rigidBody = GetComponent<Rigidbody>();
         m_viewCam = Camera.main;
+
+        KeyCodeEventMapper.Instance.AddMappedKey(KeyCode.None, KeyCode.UpArrow, KeyState.HELD, () => { m_moveDir.z += 1; }, "SimpleCamera2DMover -> Update the camera");
+        KeyCodeEventMapper.Instance.AddMappedKey(KeyCode.None, KeyCode.DownArrow, KeyState.HELD, () => { m_moveDir.z -= 1; }, "SimpleCamera2DMover -> Update the camera");
+
+        KeyCodeEventMapper.Instance.AddMappedKey(KeyCode.None, KeyCode.RightArrow, KeyState.HELD, () => { m_moveDir.x += 1; }, "SimpleCamera2DMover -> Update the camera");
+        KeyCodeEventMapper.Instance.AddMappedKey(KeyCode.None, KeyCode.LeftArrow, KeyState.HELD, () => { m_moveDir.x -= 1; }, "SimpleCamera2DMover -> Update the camera");
+
+        FunctionUpdater.CreateUpdater(() => { Move(m_moveDir); }, "SimpleCamera2dMover - Move", true, UpdateCycle.LATE);
+    }
+
+    private void Move(Vector3 dir)
+    {
+        transform.position += m_moveDir * m_moveSpeed * Time.deltaTime;
+
+        m_moveDir = Vector3.zero;
     }
 
     // Update is called once per frame
@@ -27,8 +44,6 @@ public class SimpleController : MonoBehaviour
 
         if (m_screenCentreBasedFacing) FaceMouse(_screenCentre);
         else FaceMouse(_transformScreenPosition);
-
-        m_velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * m_moveSpeed;
     }
 
     private void FaceMouse(Vector3 anchorPosition)
@@ -40,13 +55,13 @@ public class SimpleController : MonoBehaviour
         _mouseDir = Quaternion.Euler(90, 0, 0) * _mouseDir;
         _mouseDir.y = 0;
 
-        //Debug.DrawRay(transform.position, transform.position + (_mouseDir * 10));
-
         transform.forward = _mouseDir;
     }
 
     private void FixedUpdate()
     {
-        m_rigidBody.MovePosition(m_rigidBody.position + (m_velocity * Time.fixedDeltaTime));
+        m_rigidBody.MovePosition(m_rigidBody.position + (m_moveDir * m_moveSpeed * Time.fixedDeltaTime));
+
+        m_moveDir = Vector3.zero;
     }
 }
